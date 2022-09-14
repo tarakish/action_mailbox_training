408e9ec6653f3c80221c500c281a2fe1c7936030
tarakish <akr.3838soccer14@gmail.com>
2022-09-14 19:51:02 +0900

    Feat: CommnetMailboxのテスト追加

--- test/mailboxes/comments_mailbox_test.rb
+++ test/mailboxes/comments_mailbox_test.rb
@@ -1,11 +1,19 @@
 require "test_helper"
 
 class CommentsMailboxTest < ActionMailbox::TestCase
-  # test "receive mail" do
-  #   receive_inbound_email_from_mail \
-  #     to: '"someone" <someone@example.com>',
-  #     from: '"else" <else@example.com>',
-  #     subject: "Hello world!",
-  #     body: "Hello?"
-  # end
+  test "メールの本文が対応するBoardのコメントとして正しく追加されること" do
+    board = Board.create
+    user = User.create(email: 'test@example.com')
+
+    assert_difference -> { board.comments.count } do
+      receive_inbound_email_from_mail \
+        to: "#{board.id}-comment@example.com",
+        from: user.email,
+        subject: "メールタイトル",
+        body: "こんにちは！"
+    end
+    comment.board = comments.last
+    assert_equal user, comment.creator
+    assert_equal "こんにちは！", comment.body
+  end
 end
